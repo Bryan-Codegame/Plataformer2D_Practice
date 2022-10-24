4230c2d29b1802d0d6d0f2454fe0cddb4cd31338
Bryan-Codegame <bryanesc26@gmail.com>
2022-10-24 10:27:46 -0500

    Shoot Arrow works out

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, new Vector3(0, 0, 1));
+    }
+}
